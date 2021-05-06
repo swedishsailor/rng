18c587bd3798f464a1ad9fedee6dbdd2a3a84b96
swedishsailor <kzygowski7@gmail.com>
2021-05-06 14:23:32 +0200

    need to make $amount nadp layerChoice

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
 'use strict';
 
+
+           /*  FUNCTIONS   */
+
+           function highLow (playerChoice, goldAmount) {
+                playerChoice = document.getElementById('playerChoice');
+           }
+           console.log(playerChoice);
+
+
 function roll () {
     let randomNumber  = Math.floor(Math.random()*6 + 1);
+    console.log(randomNumber);
+    clearMessages();
+    printMessage('Rolled number is: ' + randomNumber + '!');
     return randomNumber;
 }
+
+
+function printMessage(msg) {
+    var div = document.createElement('div');
+    div.innerHTML = msg;
+    document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+    document.getElementById('messages').innerHTML='';
+} 
+
+
+                /* METHODS   */
+
+let rollButton = document.getElementById('roll-button');
+
+rollButton.addEventListener('click', function(){roll()});
+
