efac97a431e859ae3ea1d032f738545b339d54fa
swedishsailor <kzygowski7@gmail.com>
2021-05-10 23:51:42 +0200

    color changes on result, new chooseNr func

--- js/script.js
+++ js/script.js
@@ -14,59 +14,52 @@
 
            function bet() {
             gold = document.getElementById('goldAmount').value;
+            if (gold ==undefined || gold ==null){
+                gold = 0;
+                return;
+            }
+            if ((gold > actualBalance) || (gold < 0)){
+                alert('Wrong amount of money!');
+                return;
+            }
+            balance.innerHTML = 'Your balance: ' + actualBalance + '$, current bet is: ' + gold;
             console.log(gold);
         }
 
            function oneTap () {
-              
+            const result = document.getElementById('messages2');
                if ((nr == 1 && randomNumber == 1) || (nr == 2 && randomNumber == 2)|| (nr == 3 && randomNumber == 3)|| (nr ==4 && randomNumber ==4)|| (nr == 5 && randomNumber == 5)|| (nr == 6 && randomNumber == 6)) {
+                result.classList.remove('resultLose');
+                   result.classList.add('resultWin');
                     console.log('Wygrałeś warjacie, esa');
                     actualBalance = actualBalance + gold*5;
                     clearMessages2();
                     printMessage2('D: You won!');
                } else {
+                result.classList.remove('resultWin');
+                result.classList.add('resultLose');
                    console.log('buda, try next time');
                    clearMessages2();
                    printMessage2('Buuuda, try again');
                }
-               balance.innerHTML = 'Your balance: ' + actualBalance + '$';
+               balance.innerHTML = 'Your balance: ' + actualBalance + '$, current bet is: ' + gold;
            }
           
-                                /* Łopatologic Numbers xd */
-           function number2() {
-            nr = 2;
-            console.log('Numer wybrany to: ' + nr);
-            
-        }
 
-        function number3() {
-            nr = 3;
+        function chooseNumber(nr) {
+            if (nr==1){
             console.log('Numer wybrany to: ' + nr);
-            
-        }
-
-        function number4() {
-            nr = 4;
+            } else if (nr==2){
             console.log('Numer wybrany to: ' + nr);
-            
-        }
-
-        function number5() {
-            nr = 5;
+            }  else if (nr==3){
             console.log('Numer wybrany to: ' + nr);
-            
-        }
-
-        function number6() {
-            nr = 6;
+            }  else if (nr==4){
             console.log('Numer wybrany to: ' + nr);
-            
-        }
-
-        function number1() {
-            nr = 1;
+            }  else if (nr==5){
             console.log('Numer wybrany to: ' + nr);
-            
+            }  else if (nr==6){
+            console.log('Numer wybrany to: ' + nr);
+            } 
         }
 
            function getGoldAmount (goldAmount){
@@ -79,14 +72,21 @@
 
 
 function roll () {
-    actualBalance = actualBalance - gold;
-    console.log(actualBalance);
-    randomNumber  = Math.floor(Math.random()*6 + 1);
-    console.log(randomNumber);
-    clearMessages();
-    printMessage('Rolled number is: ' + randomNumber + '!');
-    balance.innerHTML = 'Your balance: ' + actualBalance + '$';
-    return  randomNumber, actualBalance;
+    if (gold ==undefined || gold ==null){
+        gold = 0;
+    }
+    if ((gold > actualBalance) || (gold < 0) || (gold== undefined)){
+        return;
+    } else {
+        actualBalance = actualBalance - gold;
+        console.log(actualBalance);
+        randomNumber  = Math.floor(Math.random()*6 + 1);
+        console.log(randomNumber);
+        clearMessages();
+        printMessage('Rolled number is: ' + randomNumber + '!');
+        balance.innerHTML = 'Your balance: ' + actualBalance + '$';
+        return  randomNumber, actualBalance;
+    }
 }
 
 function printMessage(msg) {
@@ -115,12 +115,12 @@ function clearMessages2(){
 balance.innerHTML = 'Your balance: ' + actualBalance + '$';
 rollButton.addEventListener('click', function(){roll()});
 rollButton.addEventListener('click', function(){oneTap()});
-playerChoice[1].addEventListener('click', function(){number2(nr)});
-playerChoice[2].addEventListener('click', function(){number3(nr)});
-playerChoice[3].addEventListener('click', function(){number4(nr)});
-playerChoice[4].addEventListener('click', function(){number5(nr)});
-playerChoice[5].addEventListener('click', function(){number6(nr)});
-playerChoice[0].addEventListener('click', function(){number1(nr)});
+playerChoice[0].addEventListener('click', function(){chooseNumber(nr = 1)});
+playerChoice[1].addEventListener('click', function(){chooseNumber(nr = 2)});
+playerChoice[2].addEventListener('click', function(){chooseNumber(nr = 3)});
+playerChoice[3].addEventListener('click', function(){chooseNumber(nr = 4)});
+playerChoice[4].addEventListener('click', function(){chooseNumber(nr = 5)});
+playerChoice[5].addEventListener('click', function(){chooseNumber(nr = 6)});
 goldAmount.addEventListener('change', function(){bet()});
 console.log(nr);
 //console.log(chosenNr);
